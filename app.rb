3610374b1d89c13d0b192633a5d753f0daeb8a2e
Coda <sopurani885@gmail.com>
2016-07-28 17:45:10 +0900

    add pict

--- app.rb
+++ app.rb
@@ -3,4 +3,7 @@ require 'sinatra'
 
 get "/" do
   "Hello world!"
+
+  @item = "http://fujifilm.jp/personal/digitalcamera/x/fujinon_lens_xf16mmf14_r_wr/sample_images/img/index/ff_xf16mmf14_r_wr_004.JPG"
+  @item.image.body
 end
