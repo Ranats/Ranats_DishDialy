89423554814d83f286da87ddafddf0d8350eeae6
Coda <sopurani885@gmail.com>
2016-07-28 18:01:11 +0900

    add pict

--- app.rb
+++ app.rb
@@ -1,9 +1,10 @@
 require 'rubygems'
 require 'sinatra'
+require 'erb'
 
 get "/" do
   "Hello world!"
 
-  @item = "http://fujifilm.jp/personal/digitalcamera/x/fujinon_lens_xf16mmf14_r_wr/sample_images/img/index/ff_xf16mmf14_r_wr_004.JPG"
-  @item.image.body
+  @src = "http://fujifilm.jp/personal/digitalcamera/x/fujinon_lens_xf16mmf14_r_wr/sample_images/img/index/ff_xf16mmf14_r_wr_004.JPG"
+  erb :index
 end
